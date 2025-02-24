96d3a518ac76280a62509a154fbc9e59fec97f7d
nidhal.essghaier <nidhal.essghaier@tu-dortmund.de>
2025-02-24 16:22:58 +0100

    added two test methods in EndTurnTest.kt

--- src/test/kotlin/service/EndTurnTest.kt
+++ src/test/kotlin/service/EndTurnTest.kt
@@ -1,5 +1,7 @@
 package service
-
+import entity.Card
+import entity.CardSuit
+import entity.CardValue
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -46,6 +48,7 @@ class EndTurnTest {
         refreshable.reset()
     }
 
+
     /**
      * Tests that the game ends when both players' hands and draw stacks are empty.
      */
@@ -90,12 +93,59 @@ class EndTurnTest {
      * Tests that the game ends when the pass counter reaches two.
      */
     @Test
-    fun testEndTurnEndsGameWhenPassCounterIsTwo() {
+    fun testPassCounterIsTwoBothWins() {
         val name1 = "Player1"
         val name2 = "Player2"
 
         gameService.startNewGame(name1, name2)
         val game = rootService.currentGame ?: fail("The game should be started.")
+        game.passCounter = 2
+
+        assertDoesNotThrow {
+            gameService.endTurn()
+        }
+        assertNull(rootService.currentGame) { "The game should be over after this turn." }
+        assertTrue(refreshable.refreshAfterEndGameCalled)
+        refreshable.reset()
+    }
+    /**
+     * Tests that the game ends when the pass counter reaches two and the active player has fewer card in hand.
+     * active player should win
+     */
+    @Test
+    fun testPassCounterIsTwoActivePlayerWins() {
+        val name1 = "Player1"
+        val name2 = "Player2"
+
+        gameService.startNewGame(name1, name2)
+        val game = rootService.currentGame ?: fail("The game should be started.")
+        game.currentPlayer = 1
+        val waitingPlayer=game.player2
+        waitingPlayer.hand.add(Card(CardSuit.SPADES, CardValue.SEVEN))
+        game.passCounter = 2
+
+        assertDoesNotThrow {
+            gameService.endTurn()
+        }
+        assertNull(rootService.currentGame) { "The game should be over after this turn." }
+        assertTrue(refreshable.refreshAfterEndGameCalled)
+        refreshable.reset()
+    }
+
+    /**
+     * Tests that the game ends when the pass counter reaches two and the waiting player has fewer card in hand.
+     * waiting player should win
+     */
+    @Test
+    fun testPassCounterIsTwoWaitingPlayerWins() {
+        val name1 = "Player1"
+        val name2 = "Player2"
+
+        gameService.startNewGame(name1, name2)
+        val game = rootService.currentGame ?: fail("The game should be started.")
+        game.currentPlayer = 1
+        
+        game.player1.hand.add(Card(CardSuit.SPADES, CardValue.SEVEN))
         game.passCounter = 2
 
         assertDoesNotThrow {
